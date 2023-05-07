c4f39c59e6674ef16fad96b4ce9d00f1a61ffff3
JSY <sjiang47@jh.edu>
2023-05-07 10:47:54 -0400

    rm run.sh

--- run.sh
+++
@@ -1,3 +0,0 @@
-./../../pox.py log.level --DEBUG Pox_Load_Balancer.controller --servers_ip=10.0.0.1,10.0.0.2,10.0.0.3 --clients_ip=10.0.1.1,10.0.1.2,10.0.1.3,10.0.1.4,10.0.1.5,10.0.1.6 --monitor_ip=10.0.3.1 --policy=2
-dump
-sudo mnexec -a [PID] bash
